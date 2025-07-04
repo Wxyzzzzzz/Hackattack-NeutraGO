0bedf5e94b5c2fd1c885325276d0c73703242895
ceeee33 <118290498+ceeee33@users.noreply.github.com>
2025-07-04 21:28:48 +0800

    Update rout_api_call.dart

--- lib/screens/rout_api_call.dart
+++ lib/screens/rout_api_call.dart
@@ -35,8 +35,8 @@ class _RunningModelState extends State<RunningModel> {
   }
 
   Future<void> fetchRecommendation() async {
-    final url = Uri.parse("http://192.168.100.6:8000/recommend"); // replace with actual backend URL
-    // final url = Uri.parse("https://route-suggest.onrender.com/recommend");
+    //final url = Uri.parse("http://192.168.100.6:8000/recommend"); // replace with actual backend URL
+    final url = Uri.parse("https://route-suggest.onrender.com/recommend");
 
     final body = {
       "user_id": "jAENInMkzS0KvYyVSyJA",
